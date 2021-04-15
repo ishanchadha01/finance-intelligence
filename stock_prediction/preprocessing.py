e357784adc0cf071ad1ce0cf3942b79a3286b31e
Ishan <ishanchadha01@gmail.com>
2021-04-15 14:51:41 -0400

    ema preprocessing

--- stock_prediction/preprocessing.py
+++ stock_prediction/preprocessing.py
@@ -35,8 +35,8 @@ def preprocessing_1year():
 
             # Bollinger bands with 20 day window using [distance to upper band, distance to lower band]
             data_dict[name]['bollinger_bands'] = []
-            for day, price in enumerate(data_dict[name][price]):
-                past_20 = data_dict[name][price][max(0, day-19) : day+1]
+            for day, price in enumerate(data_dict[name]['price']):
+                past_20 = data_dict[name]['price'][max(0, day-19) : day+1]
                 mu = sum(past_20) / len(past_20)
                 var = sum((x - mu) ** 2 for x in past_20) / len(past_20)
                 upper_band = mu + 2*var
@@ -44,7 +44,35 @@ def preprocessing_1year():
                 data_dict[name]['bollinger_bands'].append([abs(upper_band - price), abs(lower_band - price)])
 
             # Exponential moving average with [12 day EMA, 26 day EMA, 50 day EMA]
-            data_dict[name]['ema'] = []
+            data_dict[name]['ema'] = [[price, price, price] for price in data_dict[name][price]]
+            for day, price in enumerate(data_dict[name]['price']):
+                smoothing_12 = 2/(1 + 12)
+                smoothing_26 = 2/(1 + 26)
+                smoothing_50 = 2/(1 + 50)
+
+                if day == 13:
+                    ema_base = price
+                    for day_iter in range(day-1, day-14, -1):
+                        ema_base = smoothing_12 * data_dict[name]['price'][day_iter] + (1-smoothing_12) * ema_base
+                    data_dict[name]['ema'][day][0] = ema_base
+                elif day > 13:
+                    data_dict[name]['ema'][day][0] = smoothing_12 * price + (1-smoothing_12) * data_dict[name]['ema'][day-1][0]
+
+                if day == 27:
+                    ema_base = price
+                    for day_iter in range(day-1, day-28, -1):
+                        ema_base = smoothing_26 * data_dict[name]['price'][day_iter] + (1-smoothing_26) * ema_base
+                    data_dict[name]['ema'][day][1] = ema_base
+                elif day > 27:
+                    data_dict[name]['ema'][day][1] = smoothing_26 * price + (1-smoothing_26) * data_dict[name]['ema'][day-1][1]
+
+                if day == 51:
+                    ema_base = price
+                    for day_iter in range(day-1, day-52, -1):
+                        ema_base = smoothing_50 * data_dict[name]['price'][day_iter] + (1-smoothing_50) * ema_base
+                    data_dict[name]['ema'][day][2] = ema_base
+                elif day > 51:
+                    data_dict[name]['ema'][day][2] = smoothing_50 * price + (1-smoothing_50) * data_dict[name]['ema'][day-1][2]
 
             # ARIMA
             data_dict[name]['arima'] = []
